5d3dd3b45b9d749a12b8350fe5ea5a5eeedd2a27
Maria Jose Alcaraz <marialcaraz.192@gmail.com>
2025-07-29 12:31:30 +0200

    Añado nuevas columnas

--- userapp-backend/src/main/java/com/springboot/backend/maria/userapp/userappbackend/entities/User.java
+++ userapp-backend/src/main/java/com/springboot/backend/maria/userapp/userappbackend/entities/User.java
@@ -2,6 +2,7 @@ package com.springboot.backend.maria.userapp.userappbackend.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 import jakarta.persistence.Id;
@@ -11,75 +12,106 @@ import jakarta.persistence.Table;
 @Table(name = "users")
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = IDENTITY)
+  private Long id;
 
-    private String name;
-    private String lastname;
-    private String email;
-    private String username;
-    private String password;
+  private String name;
+  private String lastname;
+  private String email;
+  private String username;
+  private String password;
+  private String address;
+  private String phone;
+  //private String role;
+  private String image;
 
-    // Nuevo campo para imagen
-    private String image; // Puede almacenar una URL, ruta relativa o base64
+  // Constructores
+  public User() {
+  }
 
-    // Constructores
-    public User() {
-    }
+  public User(Long id, String name, String lastname, String email, String username, String password, String address, String phone, String image) {
+    this.id = id;
+    this.name = name;
+    this.lastname = lastname;
+    this.email = email;
+    this.username = username;
+    this.password = password;
+    this.address = address;
+    this.phone = phone;
+    this.image = image;
+  }
 
-    public User(Long id, String name, String lastname, String email, String username, String password, String image) {
-        this.id = id;
-        this.name = name;
-        this.lastname = lastname;
-        this.email = email;
-        this.username = username;
-        this.password = password;
-        this.image = image;
-    }
+  // Getters y Setters
+  public Long getId() {
+    return id;
+  }
 
-    // Getters y Setters
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getLastname() {
-        return lastname;
-    }
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    public String getImage() {
-        return image;
-    }
-    public void setImage(String image) {
-        this.image = image;
-    }
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public void setLastname(String lastname) {
+    this.lastname = lastname;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getImage() {
+    return image;
+  }
+
+  public void setImage(String image) {
+    this.image = image;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
 }
